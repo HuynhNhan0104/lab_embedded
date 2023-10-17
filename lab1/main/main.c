8010d65cbac05bbb759b4b31a36ee6a2f9e2ae72
Huynh Nguyen Hieu Nhan <hieunhantv2002@gmail.com>
2023-10-17 12:17:17 +0700

    add lab1 and lab2

---
+++ lab1/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+
+
+
+void app_main(void)
+{
+    printf("Hello world! \n");
+    
+
+}
